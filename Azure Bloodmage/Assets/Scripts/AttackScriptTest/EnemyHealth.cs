ea9aefd2f334dd6dd5dd7e19f922f9415e70109a
bbgun30 <32913497+bbgun30@users.noreply.github.com>
2019-03-25 11:16:40 -0400

    Add files via upload

---
+++ Azure Bloodmage/Assets/Scripts/AttackScriptTest/EnemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+
+    public float Health = 25f;
+
+    public void TakeDamage(float amnt)
+    {
+        Health -= amnt;
+        if (Health <= 0)
+        {
+            print("Enemy Has died!!!");
+        }
+
+        print("Enemy took some damage");
+    }
+
+}
